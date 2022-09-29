adf528354155ccad9b8d8713c272e5041795560a
Emre Eren <emrere@hotmail.com>
2022-09-29 18:31:25 +0300

    oldu mu babuş

--- JS/main.js
+++ JS/main.js
@@ -3,7 +3,7 @@ var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
     var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
+    var txt = document.createTextNode("");
     span.className = "close";
     span.appendChild(txt);
     myNodelist[i].appendChild(span);
@@ -41,7 +41,7 @@ function newElement() {
     document.getElementById("myInput").value = "";
 
     var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
+    var txt = document.createTextNode("");
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
